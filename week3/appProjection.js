19d3be09e6fc08001f0c32d5defae5966ed1e4fe
bruno-reis <bruno.reis@useboom.net>
2017-01-29 12:25:51 -0200

    added query params and regex lessons files

--- week3/appProjection.js
+++ week3/appProjection.js
@@ -7,15 +7,21 @@ MongoClient.connect('mongodb://localhost:27017/crunchbase', (err, db) => {
   console.log("Successfully connected");
 
   const query = {"category_code": "biotech"};
+  const projection = {"name": 1, "category_code": 1, "_id": 0};
+
   const cursor = db.collection('companies').find(query);
+  cursor.project(projection);
 
   cursor.forEach(
-    doc => console.log(`${doc.name} is a ${doc.category_code} company.`),
+    doc => {
+      console.log(`${doc.name} is a ${doc.category_code} company.`);
+      console.log(doc);
+    },
     err => {
       assert.equal(err, null);
       db.close();
     }
   );
-  
+
 });
 
